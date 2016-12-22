9b36a8f1ac678f0a03e3e44b5053a20ef469e80a
Peter Kolski <peter@bildpeter.de>
2016-12-22 10:05:26 +0100

    Grid class

---
+++ Lights/src/Grid.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Peter A. Kolski on 22.12.16.
+//
+
+#include "Grid.h"
+
+kata::Grid::Grid( int sizeX, int sizeY )
+{
+
+}
---
+++ Lights/src/Grid.h
@@ -0,0 +1,23 @@
+//
+// Created by Peter A. Kolski on 22.12.16.
+//
+
+#ifndef OPENFRAMEWORKS_GRID_H
+#define OPENFRAMEWORKS_GRID_H
+
+#include <vector>
+
+namespace kata
+{
+class Grid
+{
+public:
+    Grid(  int sizeX, int sizeY );
+
+private:
+    std::vector< std::vector< bool > >      grid_;
+};
+}
+
+
+#endif //OPENFRAMEWORKS_GRID_H
