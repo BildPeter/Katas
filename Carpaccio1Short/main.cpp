a14bba37b4ad233b60cffe4e1a06b85f99ef9b4c
Peter Kolski <peter@bildpeter.de>
2017-02-14 11:57:06 +0100

    GUI and country setting

--- Carpaccio1Short/main.cpp
+++ Carpaccio1Short/main.cpp
@@ -1,36 +1,50 @@
 #include "ofMain.h"
-
-
+#include "ofxGui.h"
+#include <unordered_map>
 
 
 
 class ofApp : public ofBaseApp{
 public:
-	int amountProduct = 0;
+    ofxIntSlider amountProduct;
+    ofxPanel gui;
+
+//	int amountProduct = 0;
 	double priceSingle = 0;
 	double priceFinal = 0.0;
 	double taxPercentageCurrent = 15;
+	string country = "";
+
+	std::unordered_map< string, double > CountryToTaxPercentage =
+			{
+				{ "DE", 15.0 }
+			};
 
 
-	double getTaxFactor() const { return ( taxPercentageCurrent / 100 + 1 ); }
-
-	double getFinalPrice() const { return priceSingle * amountProduct * getTaxFactor(); }
+	double getTaxFactor( string countryCode ) { return ( CountryToTaxPercentage.at( countryCode ) / 100 + 1 ); }
+	double getFinalPrice( string countryCode ) { return priceSingle * amountProduct * getTaxFactor( countryCode ); }
 
 	void setup()
 	{
-		amountProduct = 1;
+        gui.setup( "Shop", "guiSettings.xml", 200, 50 );
+		gui.add( amountProduct.setup( "Amount", 1, 0, 100000 ) );
+//		amountProduct = 1;
 		priceSingle = 10.0;
+		country = "DE";
 	}
 
 	void update()
-	{}
+	{
+	}
 
 	void draw()
 	{
-		ofDrawBitmapStringHighlight( "Amount: " + ofToString( amountProduct ), 10, 10 );
+//		ofDrawBitmapStringHighlight( "Amount: " + ofToString( amountProduct ), 10, 10 );
 		ofDrawBitmapStringHighlight( "Tax: " + ofToString( taxPercentageCurrent ) + "%", 10, 30 );
 		ofDrawBitmapStringHighlight( "Price single: " + ofToString( priceSingle ) + "EURO", 10, 50 );
-		ofDrawBitmapStringHighlight( "Price Final: " + ofToString( getFinalPrice() ), 10, 300, ofColor::red );
+		ofDrawBitmapStringHighlight( "Price Final: " + ofToString( getFinalPrice( country ) ), 10, 300, ofColor::red );
+
+		gui.draw();
 	}
 };
 
