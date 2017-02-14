813235fe9652fb4172e920993d932b43ed778fc2
Peter Kolski <peter@bildpeter.de>
2017-02-14 12:22:41 +0100

    Reduction

--- Carpaccio1Short/main.cpp
+++ Carpaccio1Short/main.cpp
@@ -30,12 +30,30 @@ public:
 			};
 
 	double getTaxFactor( string countryCode ) { return ( CountryToTaxPercentage.at( countryCode ) / 100 + 1 ); }
-	double getFinalPrice( string countryCode ) { return priceSingle * amountProduct * getTaxFactor( countryCode ); }
+	double getReductionPercentage( int amount )
+	{
+		double percentage = 0.0;
+		if ( amount >= 50000 ) percentage = 15.0;
+		else if ( amount >= 10000 ) percentage = 10.0;
+		else if ( amount >= 7000 ) percentage = 7.0;
+		else if ( amount >= 5000 ) percentage = 5.0;
+		else if ( amount >= 1000 ) percentage = 3.0;
+
+		return percentage;
+	}
+	double getFinalPrice( string countryCode )
+	{
+		double priceBeforeReduction = priceSingle * amountProduct * getTaxFactor( countryCode );
+		double reductionFactor = 1.0;
+
+		return priceBeforeReduction * reductionFactor;
+	}
+	
 
 	void setup()
 	{
         gui.setup( "Shop", "guiSettings.xml", 200, 50 );
-		gui.add( amountProduct.setup( "Amount", 1, 0, 100000 ) );
+		gui.add( amountProduct.setup( "Amount", 1, 0, 70000 ) );
 		priceSingle = 10.0;
 		country = "GB";
 	}
@@ -48,6 +66,7 @@ public:
 	{
 		ofDrawBitmapStringHighlight( "Tax: " + ofToString( CountryToTaxPercentage.at( country ) ) + "%", 10, 30 );
 		ofDrawBitmapStringHighlight( "Price single: " + ofToString( priceSingle ) + "EURO", 10, 50 );
+		ofDrawBitmapStringHighlight( "Reduction Percentage: " + ofToString( getReductionPercentage( amountProduct ) ) + "%", 10, 70 );
 		ofDrawBitmapStringHighlight( "Price Final: " + ofToString( getFinalPrice( country ) ), 10, 300, ofColor::red );
 
 		gui.draw();
