85a35fd31d7c481da29010ec075772e03854edf1
Peter Kolski <peter@bildpeter.de>
2017-09-30 22:17:39 +0200

    Single Resposibility Example

--- SWIFT/Single_Resposibility_Principle/Sources/main.swift
+++ SWIFT/Single_Resposibility_Principle/Sources/main.swift
@@ -1,1 +1,46 @@
-print("Hello, World!")
+class Journal
+{
+    var entries = [String]()
+    var count = 0
+
+    func addEntry(_ text: String) -> Int
+    {
+        count += 1
+        entries.append("\(count): \(text)")
+        return count - 1
+    }
+
+    func removeEntry(_ id: Int)
+    {
+        entries.remove(at: id)
+    }
+
+    func getEntry(_ id: Int) -> String
+    {
+        return entries[id]
+    }
+
+    func printAll()
+    {
+        for item in entries
+        {
+            print(item)
+        }
+    }
+}
+
+func main()
+{
+    let j = Journal()
+    let _ = j.addEntry("I love my iPad")        //TODO What does that mean?
+    let bug = j.addEntry("Noone is in Lacuna")
+
+    print(j.getEntry(0))
+    print(j.getEntry(1))
+    print("======")
+
+    j.removeEntry(1)
+    j.printAll()
+}
+
+main()
