d38e2266b1fe360160881abdc2e7d30b24d9ed22
Peter Kolski <peter@bildpeter.de>
2017-10-08 14:05:36 +0200

    Basic Playground

---
+++ SWIFT/Basics.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+// -------- ARRAY
+var array : [String] = ["str1", "str2", "str3" ]
+
+if array.contains("str1") {
+    print("geil einfach")
+} else {
+    print("doch nicht")
+}
+
+// -------- DICTIONARY
+var names = [ 1 : "Peter",  2 : "Marcel" ]
+names[1]
+
+// -------- Optional Type
+// -------- Implicitly Unwrapped Optional Type
+// -------- Protocol Composition Type
+
+// --------
