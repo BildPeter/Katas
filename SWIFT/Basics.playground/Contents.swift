5262acb3b2e70071438a918f609c1be89b79f2d1
Peter Kolski <peter@bildpeter.de>
2017-10-08 15:06:23 +0200

    State machine example

--- SWIFT/Basics.playground/Contents.swift
+++ SWIFT/Basics.playground/Contents.swift
@@ -2,6 +2,10 @@
 
 import Cocoa
 
+// -------- Type renaming = Type identifier
+typealias Point = (Int, Int)
+let pos : Point = ( 2, 7 )
+
 // -------- ARRAY
 var array : [String] = ["str1", "str2", "str3" ]
 
@@ -15,8 +19,13 @@ if array.contains("str1") {
 var names = [ 1 : "Peter",  2 : "Marcel" ]
 names[1]
 
+// -------- Tuple Type
+typealias StateTransition = ( trigger:Int, state:Int )
+var stateTrans1 : StateTransition = ( trigger:1, state:7)
+var stateTrans2 : StateTransition = ( 0, 3 )
+
 // -------- Optional Type
 // -------- Implicitly Unwrapped Optional Type
 // -------- Protocol Composition Type
 
-// --------
+// -------- https://developer.apple.com/library/content/documentation/Swift/Conceptual/Swift_Programming_Language/Types.html#//apple_ref/swift/grammar/tuple-type
---
+++ SWIFT/StatePattern/StatePattern/main.swift
@@ -0,0 +1,75 @@
+//
+//  main.swift
+//  StatePattern
+//
+//  Created by Peter A. Kolski on 08.10.17.
+//  Copyright © 2017 Peter Kolski. All rights reserved.
+//
+
+//import Foundation
+//
+//print("Hello, World!")
+
+import Foundation
+//import XCTest
+
+enum State
+{
+    case offHook
+    case connecting
+    case connected
+    case onHold
+}
+
+enum Trigger
+{
+    case callDialed
+    case hungUp
+    case callConnected
+    case placedOnHold
+    case takenOffHold
+    case leftMessage
+}
+
+let rules = [
+    State.offHook: [
+        (Trigger.callDialed, State.connecting)
+    ],
+    State.connecting: [
+        (Trigger.hungUp, State.offHook),
+        (Trigger.callConnected, State.connected)
+    ],
+    State.connected: [
+        (Trigger.leftMessage, State.offHook),
+        (Trigger.hungUp, State.offHook),
+        (Trigger.placedOnHold, State.onHold)
+    ],
+    State.onHold: [
+        (Trigger.takenOffHold, State.connected),
+        (Trigger.hungUp, State.offHook)
+    ]
+]
+
+func main()
+{
+    var state = State.offHook // starting state
+    while true
+    {
+        print("The phone is currently \(state)")
+        print("Select a trigger:")
+
+        for i in 0..<rules[state]!.count
+        {
+            let (t, _) = rules[state]![i]
+            print("\(i). \(t)")
+        }
+
+        if let input = Int(readLine()!)
+        {
+            let (_, s) = rules[state]![input]
+            state = s
+        }
+    }
+}
+
+main()
