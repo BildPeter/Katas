d1f480993e73380c7198ee1d54fc46479ad47884
Peter Kolski <peter@bildpeter.de>
2017-11-30 08:51:33 +0100

    created a basic swift project

---
+++ Basics/swiftBasics/swiftBasics/main.swift
@@ -0,0 +1,23 @@
+//
+//  main.swift
+//  swiftBasics
+//
+//  Created by Peter A. Kolski on 30.11.17.
+//  Copyright © 2017 Peter Kolski. All rights reserved.
+//
+
+import Foundation
+
+class Printer
+{
+    func printHallo()
+    {
+        print( "Hallo Peter")
+    }
+}
+
+
+let p = Printer()
+
+p.printHallo()
+
