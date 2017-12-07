393aa7d26ca4e88bbe8cb30cc69b7ff9f5a7db37
Peter Kolski <peter@bildpeter.de>
2017-12-07 13:36:28 +0100

    Some basic variables in swift

--- Basics/swiftBasics/swiftBasics/main.swift
+++ Basics/swiftBasics/swiftBasics/main.swift
@@ -12,7 +12,7 @@ class Printer
 {
     func printHallo()
     {
-        print( "Hallo Peter")
+        print( "Hallo Peter" )
     }
 }
 
@@ -20,4 +20,45 @@ class Printer
 let p = Printer()
 
 p.printHallo()
+
+
+// #################################################################
+// ##################### Datatypes #################################
+// #################################################################
+
+// ---------------------- ARRAYS -----------------------------------
+var someArray = [ Int ]( repeating: 7, count: 3 )
+var someVar   = someArray[ 1 ]
+print( "Array value: \( someVar )" )
+//Adding an element
+someArray.append( 20 )
+someArray += [ 40 ]
+
+// ---------------------- DICTIONARIES -----------------------------
+
+var someDict : [ Float : String ] = [ 1.0: "One", 2.0: "Two", 3.0: "Three" ]
+var someVar1                      = someDict[ 2.0 ]
+var someVar2                      = someDict[ 1 ]
+print( "2.0 = \( someVar1 ) || 1 = \( someVar2 )" )
+
+for ( key, value ) in someDict
+{
+    print( "Dictionary key \( key ) - Dictionary value \( value )" )
+}
+someDict.count
+someDict.isEmpty
+
+//Enum
+
+
+//Optionals
+//Without value
+var perhapsInt : Int?
+
+
+
+// -------------------------- DECISION ------------------------------
+var doIt   = true
+let choice = doIt ? "Ja" : "Nein"
+print( choice )
 
