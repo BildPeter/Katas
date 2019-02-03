5a978d72c6515674296e5c81f7d4bd65f85c21f5
Peter Kolski <mail@peterkolski.com>
2019-02-03 18:02:17 +0100

    New type training file

---
+++ TypeTraining/type training.swift
@@ -0,0 +1,18 @@
+
+// --- adding
+airports["LHR"] = "London"
+// --- remove
+airports["LHR"] = nil
+
+for key in dict.keys
+for value in dict.values
+
+// --- Array of keys/values
+let keys = [Int]dict.keys
+let values = [String]dict.values
+
+// --- Closures
+delayCounterRect = delayCounterRect.mapValues { $0 - 1 }
+delayCounterRect = delayCounterRect.filter { $0.value > 0 }
+
+
